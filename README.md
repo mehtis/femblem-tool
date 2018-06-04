7027622df93f0a1000e0f14166aaf6bf529f116d
Niko Mehtonen <nikomehtonen@gmail.com>
2018-06-04 15:58:16 +0300

    First commit, scrape for character name (page title)

---
+++ README.md
@@ -0,0 +1,18 @@
+# Fire Emblem Tool
+Very much WIP
+
+## Running
+``` bash
+npm run watch
+```
+Note! Automatic watch still not doe
+
+## Usage
+Go to `localhost:8081` in your browser
+You can use characterName as a query parameter (e.g. `http://localhost:8081/?characterName=Crom`) to search for different characters.
+
+## Attribution
+Data downloaded from [http://fireemblem.wikia.com/wiki/Fire_Emblem_Wiki]
+
+## License
+BSD-2-Clause
---
+++ src/server/main.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const request = require('request')
+const cheerio = require('cheerio')
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const url = req.query.characterName
+  ? `http://fireemblem.wikia.com/wiki/${req.query.characterName}`
+  : 'http://fireemblem.wikia.com/wiki/Avatar_(Awakening)'
+
+  request(url, (err, response, html) => {
+    if(!err) {
+      const $ = cheerio.load(html)
+      //TODO Scrape from a different field
+      const characterName = $('.page-header__title').text()
+
+      console.log('Sending ' + characterName)
+      res.send(characterName)
+      return
+    }
+    //TODO Not getting to error right now, page title is show even with false
+  console.err('Error loading: '+ url)
+  res.send('Error')
+  })
+})
+
+app.listen(8081, () => console.log('Listening on 8081'))
