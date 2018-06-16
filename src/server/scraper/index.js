ae4ef8702033f5bdbf892a2b4c74bd290f29b1d2
Niko Mehtonen <nikomehtonen@gmail.com>
2018-06-16 21:26:10 +0300

    Added character growth rates

--- src/server/scraper/index.js
+++ src/server/scraper/index.js
@@ -5,7 +5,17 @@ const scrape = (webpage) => {
     const $ = cheerio.load(webpage)
     const data = {
       characterName: $('.pi-title').text(),
-      startingClass: $('.statbox').first().find('td').first().find('a').last().text()
+      startingClass: $('.statbox').first().find('td').first().find('a').last().text(),
+      growthRates: {
+        hp:  growthRate($, 1),
+        str:  growthRate($, 2),
+        mag:  growthRate($, 3),
+        skl:  growthRate($, 4),
+        spd:  growthRate($, 5),
+        lck:  growthRate($, 6),
+        def:  growthRate($, 7),
+        res:  growthRate($, 8),
+      }
     }
 
     //const startingClass = $('.statbox')
@@ -15,5 +25,9 @@ const scrape = (webpage) => {
       reject(Error('Character data not found'))
     }
   })
+}
+
+const growthRate = ($, index) => {
+  return $('#Growth_Rates').parent().next().find('.s-cells').children(`td:nth-child(${index})`).text().trim()
 }
 module.exports = scrape
