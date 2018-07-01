a3d2edf6a9350e941f3f706a8cac5f1a26a9ed5a
Niko Mehtonen <nikomehtonen@gmail.com>
2018-07-01 11:11:34 +0300

    Fixes to CharacterSheet refactoring

--- client/components/CharacterSheet.js
+++ client/components/CharacterSheet.js
@@ -1,41 +1,44 @@
-import React from 'React'
+import React from 'react'
 
 class CharacterSheet extends React.Component {
+  
   render() {
-    <div className='character-sheet'>
-      <h1>{`Character name: ${this.props.characterName}`}</h1>
-      <p>{`Starting class: ${this.props.startingClass}`}</p>
-      <p>Base classes:</p>
-      <ul>
-        {this.props.baseClasses.map((value) => {
-          return <li>{`${value[0]}: ${value[1]}`}</li>
-        })}
-      </ul>
-      <p>Growth rates:</p>
-      <ul>
-        {this.props.growthRates.map((value) => {
-          return <li>{`${value[0]}: ${value[1]}`}</li>
-        })}
-      </ul>
-      <p>Max stat modifiers:</p>
-      <ul>
-        {this.props.maxStatModifiers.map((value) => {
-          return <li>{`${value[0]}: ${value[1]}`}</li>
-        })}
-      </ul>
-      <p>Romantic supports:</p>
-      <ul>
-        {this.props.romanticSupports.map((value) => {
-          return <li>{`${value}`}</li>
-        })}
-      </ul>
-      <p>Other supports:</p>
-      <ul>
-        {this.props.otherSupports.map((value) => {
-          return <li>{`${value}`}</li>
-        })}
-      </ul>
-    </div>
+    return(
+      <div className='character-sheet'>
+        <h1>{`Character name: ${this.props.characterName}`}</h1>
+        <p>{`Starting class: ${this.props.startingClass}`}</p>
+        <p>Base classes:</p>
+        <ul>
+          {this.props.baseClasses.map((value) => {
+            return <li>{`${value[0]}: ${value[1]}`}</li>
+          })}
+        </ul>
+        <p>Growth rates:</p>
+        <ul>
+          {this.props.growthRates.map((value) => {
+            return <li>{`${value[0]}: ${value[1]}`}</li>
+          })}
+        </ul>
+        <p>Max stat modifiers:</p>
+        <ul>
+          {this.props.maxStatModifiers.map((value) => {
+            return <li>{`${value[0]}: ${value[1]}`}</li>
+          })}
+        </ul>
+        <p>Romantic supports:</p>
+        <ul>
+          {this.props.romanticSupports.map((value) => {
+            return <li>{`${value}`}</li>
+          })}
+        </ul>
+        <p>Other supports:</p>
+        <ul>
+          {this.props.otherSupports.map((value) => {
+            return <li>{`${value}`}</li>
+          })}
+        </ul>
+      </div>
+    )
   }
 }
 
