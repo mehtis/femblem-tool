2e0df5ebb51475fbdf1ac00c66994223db8a104b
Niko Mehtonen <nikomehtonen@gmail.com>
2018-07-21 10:50:12 +0300

    Button now adds charactersheets. Also added babel-eslint to dev dependencies

--- client/components/Characters.js
+++ client/components/Characters.js
@@ -1,39 +1,51 @@
 import CharacterSheet from '../components/CharacterSheet'
 import characters from '../static/characters.json'
 import React from 'react'
+import uuid from 'uuid/v4'
 
-const PlusButton = () => { 
-  return (
-    <button className="plus-button">
-      +
-      <style jsx>{`
-        .plus-button {
-          border-radius: 50%;
-          float: right;
-        }
-      `}</style>
-    </button>
-  )
-}
 class Characters extends React.Component {
-
   constructor(props) {
     super(props)
-    this.state = {characterNumber: 1}
+    this.state = {characterList: []}
   }
 
-  addCharacter() {
-    //TODO: Complete
-    this.setState({characterNumber: this.state.characterNumber++})
+  componentDidMount() {
+    //TODO: WIP
+    this.setState({characterList: [ ...this.state.characterList,
+      this.props.game === 'Awakening'
+        ? {game: 'Awakening', name: 'Chrom', id: uuid()}
+        : {game: 'Fates', name: 'Ryoma', id: uuid()}
+    ]})
+  }
+
+  addCharacter = () => {
+    //TODO: WIP
+    this.setState({characterList: [ ...this.state.characterList,
+      this.props.game === 'Awakening'
+        ? {game: 'Awakening', name: 'Chrom', id: uuid()}
+        : {game: 'Fates', name: 'Ryoma', id: uuid()}
+    ]})
   }
 
   render() {
     return (
       <div className="characters">
-        <CharacterSheet character={this.props.game === 'Awakening' ? characters.Awakening.Chrom : characters.Fates.Ryoma} />
-        <PlusButton />
+        {this.state.characterList.length > 0 && this.state.characterList.map((character) => <CharacterSheet key={character.id} character={characters[character.game][character.name]} id ={character.id} />)}
+        <button className="plus-button" onClick={this.addCharacter}>
+          +
+        </button>
+        <style jsx>{`
+          .plus-button {
+            border-radius: 50%;
+            float: right;
+          }
+        `}</style>
       </div>)
   }
+}
+
+Characters.defaultProps = {
+  game: 'Awakening'
 }
 
 export default Characters
